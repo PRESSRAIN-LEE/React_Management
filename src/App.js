132db772de383ca7e5cc65e9c33993392fc28744
PRESSRAIN-LEE <pressrainlee@gmail.com>
2023-08-06 23:09:17 +0900

    5강. Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '20000101',
-  'gender': '남자',
-  'job': '대학생'
-}
+//배열
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '20000101',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김길동',
+    'birthday': '20010101',
+    'gender': '남자',
+    'job': '직장인'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이길동',
+    'birthday': '20020101',
+    'gender': '여자',
+    'job': '개발자'
+  }
+]
 
 /*
 function App() {
@@ -26,12 +47,24 @@ function App() {
 class App extends Component {
   render(){
     return (
-      <Customer
-      name = {customer.name}
-      birthday = {customer.birthday}
-      gender = {customer.gender}
-      job = {customer.job}
-    />
+      <div>
+        {
+          //map을 이용해서 for문을 만든다(python과 동일)
+          customers.map(c => {
+            return(
+              <Customer
+                key = {c.id}    //map을 사용하면 key값은 필수(unique)
+                id = {c.id}
+                image = {c.image}
+                name = {c.name}
+                birthday = {c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+              />
+            );
+          })
+        }
+      </div>
     );
   }
 }
