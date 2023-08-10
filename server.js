34ac797c41cabb9966f4aa3dcc992194e9cee567
PRESSRAIN-LEE <pressrainlee@gmail.com>
2023-08-10 14:31:16 +0900

    Create Customer DB & Node.js DB Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');	//파일 접근 라이브러리(DB연결)
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//DB연결 설정
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');	//mysql 라이브러리
+
+const connection = mysql.createConnection({
+	"host": conf.host,
+	"user": conf.user,
+	"password": conf.password,
+	"port": conf.port,
+	"database": conf.database
+});
+connection.connect();		//실제 연결
+
 app.get('/api/customers', (req, res) => {
-	res.send([
-		{
-			'id': 1,
-			'image': 'https://placekitten.com/64/64',
-			'name': '홍길동',
-			'birthday': '20000101',
-			'gender': '남자',
-			'job': '대학생'
-		  },
-		  {
-			'id': 2,
-			'image': 'https://picsum.photos/64/64',
-			'name': '김길동',
-			'birthday': '20110101',
-			'gender': '남자',
-			'job': '직장인'
-		  },
-		  {
-			'id': 3,
-			'image': 'https://loremflickr.com/64/64',
-			'name': '이길동',
-			'birthday': '20020101',
-			'gender': '여자',
-			'job': '개발자'
-		  }
-	]);
+	connection.query(
+		"SELECT * FROM customer",
+		(err, rows, fields) => {	//rows에 디비내용을 저장
+			//console.log(fields);
+			res.send(rows);
+		}
+	);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
